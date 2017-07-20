f2b3c62d478f3397d1e956c91ff1f0c07f8a2e8a
Valentin Vago <zeropaper@irata.ch>
2017-07-20 12:52:47 +0200

    chore(project): add the base files

---
+++ index.js
@@ -0,0 +1,32 @@
+$(document).ready(function () {
+  var $body = $('body');
+
+  $.getJSON('data.json', function(array) {
+    // we create a jQuery h1 element for the title
+    var $h1 = $('<h1>Welcome</h1>');
+    var $link = $('<a id="author-link"></a>');
+
+    // we create a jQuery list element for the page
+    var $ul = $('<ul id="pager"></ul>');
+
+    // for each items in the array
+    // for (var index = 0; index < array.length; index++) {}
+    array.forEach(function(item, index) {
+      // we create a jQuery list item
+      var $li = $('<li>' + (index + 1) + '</li>');
+
+      $li.on('click', function() {
+        $h1.text(item.title);
+        $body.css('background-image', 'url('+item.src+')');
+        $link
+          .text(item.author)
+          .attr('href', item.link)
+      });
+
+      // that we add to the list ($ul)
+      $ul.append($li);
+    });
+
+    $('body').append($h1, $ul, $link)
+  });
+});
